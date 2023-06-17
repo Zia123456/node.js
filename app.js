12ddbee2fa839836e8e1c964f1a4c9f0ff69bc9c
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-06-17 23:51:29 +0530

    Remove unused code

--- app.js
+++ app.js
@@ -2,22 +2,4 @@ function sayHello(){
     console.log(module);
 }
 
-sayHello();
 
-function sayBy(){
-    console.log("Bye");
-}
-
-sayBy();
-
-function greating() {
-    console.log("how are you?");
-}
-
-greating();
-
-function response() {
-    console.log("I am fine");
-}
-
-response();
