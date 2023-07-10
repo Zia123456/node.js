ae4ab3d40efa4301a37f024eed58a9df4129d2c2
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-07-10 09:34:34 -0700

    Export modules

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-console.log(__dirname);
-
-setInterval(() => {
-  console.log('Hello, world!');
-}, 1000);
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+console.log(names);
+console.log(sayHi);
+sayHi('susan');
+sayHi(names.john);
+sayHi(names.peter);
