10e99ca3c8923e67ea5be1e58e0a74657f7cc7eb
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-07-11 09:21:00 -0700

    Use built-in modules

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const names = require('./4-names');
-const sayHi = require('./5-utils');
-const data = require('./6-alternative-flaver');
-require('./7-mind-grenade');
+const os = require('os');
 
-// sayHi('susan');
-// sayHi(names.john);
-// sayHi(names.peter);
+// info about current user
+
+const user = os.userInfo();
+console.log(user);
+
+// method returns the system uptime in seconds
+
+console.log(`the syetem uptime is ${os.uptime} deconds`);
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+};
+
+console.log(currentOS);
