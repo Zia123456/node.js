c4bb31e8a14669f303b17ac64fec29c58f67c782
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-07-20 09:28:56 -0700

    Fix error

--- app.js
+++ app.js
@@ -3,17 +3,15 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
     res.end('Welcome to our home page');
-  }
-  if (req.ulr === '/about') {
+  } else if (req.url === '/about') {
     res.end('Here is our short history');
+  } else {
+    res.end(`
+      <h1>oops!</h1>
+      <p>We can't seem to find the page you are looking for</p>
+      <a href="/">Back Home</a>
+    `);
   }
-  res.end(`
-    <h1>oops!</>
-    <p>We can't seem to find the page</p>
-  `);
-  console.log(req);
-  res.write('welcome to our home page');
-  res.end();
 });
 
 server.listen(5000);
