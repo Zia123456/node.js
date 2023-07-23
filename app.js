8019f9859d5e8f97d0721e46855609973ca15a2e
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-07-23 10:11:03 -0700

    Event loop

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const _ = require('lodash');
+const { readFileSync, writeFileSync } = require('fs');
+console.log('start');
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log('hello Zia');
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
+
+console.log(first, second);
+
+writeFileSync(
+  './content/result-sync.txt',
+  `Here is the result : ${first}, ${second}`,
+  { flag: 'a' },
+);
+
+console.log('done with this task');
+console.log('starting the next one');
