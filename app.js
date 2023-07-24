6a5230f436e81ad405d5274fbbf6da8b6c1ae00d
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-07-24 11:02:42 -0700

    Test blocking code

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const { readFileSync, writeFileSync } = require('fs');
-console.log('start');
+const http = require('http');
 
-const first = readFileSync('./content/first.txt', 'utf8');
-const second = readFileSync('./content/second.txt', 'utf8');
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('Home Page');
+  } else if (req.url === '/about') {
+    for (let i = 0; i < 1000; i++) {
+      for (let j = 0; j < 1000; j++) {
+        console.log(`${i} ${j}`);
+      }
+    }
+    res.end('About Page');
+  } else {
+    res.end('Error Page');
+  }
+});
 
-console.log(first, second);
-
-writeFileSync(
-  './content/result-sync.txt',
-  `Here is the result : ${first}, ${second}`,
-  { flag: 'a' },
-);
-
-console.log('done with this task');
-console.log('starting the next one');
+server.listen(5000, () => {
+  console.log('Server Listening on port 5000...');
+});
