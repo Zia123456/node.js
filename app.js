473b4adb7bd924d2a41c03964b7e89c3850bb97d
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-07-24 11:14:49 -0700

    Use promise

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const http = require('http');
+const { readFile } = require('fs');
+const { resolve } = require('path');
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('Home Page');
-  } else if (req.url === '/about') {
-    for (let i = 0; i < 1000; i++) {
-      for (let j = 0; j < 1000; j++) {
-        console.log(`${i} ${j}`);
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
       }
-    }
-    res.end('About Page');
-  } else {
-    res.end('Error Page');
-  }
-});
+    });
+  });
+};
 
-server.listen(5000, () => {
-  console.log('Server Listening on port 5000...');
-});
+const start = async () => {
+  const first = await getText('./content/first.txt');
+};
+
+// getText('./content/first.txt')
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
