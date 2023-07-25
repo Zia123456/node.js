5d77e610a896c00dca833855ec6ce562fd7c3f39
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-07-25 11:03:32 -0700

    Async patterns

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-const { readFile } = require('fs');
-const { resolve } = require('path');
-
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+const { readFile, writeFile } = require('fs');
+const util = require('util');
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
-  const first = await getText('./content/first.txt');
+  try {
+    const first = await readFilePromise('./content/first.txt', 'utf8');
+    const second = await readFilePromise('./content/second.txt', 'utf8');
+    await writeFilePromise(
+      './content/result-mind-grenade.txt',
+      `this is awesome : ${first} ${second}`,
+    );
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf8', (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
 
 // getText('./content/first.txt')
 //   .then((result) => console.log(result))
