ed1b12f209084465d02904c06c5ee855e5f93669
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-07-26 10:48:49 -0700

    Some changes

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const { readFile, writeFile } = require('fs');
+const { readFile, writeFile } = require('fs').promises;
 const util = require('util');
 const readFilePromise = util.promisify(readFile);
 const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
   try {
-    const first = await readFilePromise('./content/first.txt', 'utf8');
-    const second = await readFilePromise('./content/second.txt', 'utf8');
+    const first = await readFile('./content/first.txt', 'utf8');
+    const second = await readFile('./content/second.txt', 'utf8');
     await writeFilePromise(
       './content/result-mind-grenade.txt',
       `this is awesome : ${first} ${second}`,
+      { flag: 'a' },
     );
     console.log(first, second);
   } catch (error) {
