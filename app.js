7208c07418f821f8ba65fcc01f6744af25d1d745
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-07-31 10:43:45 -0700

    Create server

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const { createReadStream } = require('fs');
+var http = require('http');
+var fs = require('fs');
 
-const stream = createReadStream('./content/big.txt');
-
-stream.on('data', (result) => {
-  console.log(result);
-});
+http
+  .createServer(function (req, res) {
+    // const text = fs.readFileSync('./content/big.txt', 'utf8');
+    // res.end(text);
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
+    fileStream.on('open', () => {
+      fileStream.pipe(res);
+    });
+    fileStream.on('error', (err) => {
+      res.end(err);
+    });
+  })
+  .listen(5000);
